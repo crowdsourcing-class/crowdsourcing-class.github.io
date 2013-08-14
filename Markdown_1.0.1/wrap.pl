5589017391a809b5316f4b0759530578c019e742
Chris Callison-Burch <ccb@stanfordalumni.org>
2013-08-14 12:03:45 -0400

    Markdown perl program

---
+++ Markdown_1.0.1/wrap.pl
@@ -0,0 +1,8 @@
+print "<html>\n";
+print "<head><title>Crowdsourcing and Human Computation: Special Topics CIS 399-001</title></head>\n";
+print "<body>\n";
+while($line = <>) {
+    print $line;
+}
+print "</body>\n";
+print "</html>\n";
