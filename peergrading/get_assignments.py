48c9426394a9da2c2e8fa5676de34c13c4ec333d
epavlick <elliepavlick@gmail.com>
2014-10-10 17:37:13 -0400

    code for assigning peer grading videos

---
+++ peergrading/get_assignments.py
@@ -0,0 +1,29 @@
+import sys
+import random
+
+students = [l.strip() for l in open(sys.argv[1]).readlines()]
+
+projects = {}
+
+for line in open(sys.argv[2]).readlines() : 
+	team, name, url = line.strip().split('\t')
+	if team not in projects : projects[team] = []
+	projects[team].append((name,url))
+
+
+assigned = {}
+remaining = projects.keys()
+
+for s in students : 
+	teams = 0
+	if len(remaining) < 3 : remaining = projects.keys()
+	options = [k for k in remaining if not s in k.split(',')]
+	while teams < 3 : 
+		team = random.choice(list(set(options).intersection(remaining)))
+		remaining.remove(team)
+		for n,u in projects[team] :
+			print '%s\t%s\t%s\t%s'%(s,team,n,u)
+		teams += 1
+
+
+
