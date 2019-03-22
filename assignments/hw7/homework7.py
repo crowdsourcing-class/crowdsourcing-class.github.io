ee4c1676e84862d9624787acf89208c8b6794552
Jie Gao <jiegao@nichols-ve500-0219.apn.wlan.upenn.edu>
2019-03-22 17:45:35 -0400

    Small fix in EM algorithm in the hw7 skeleton file.

--- assignments/hw7/homework7.py
+++ assignments/hw7/homework7.py
@@ -29,7 +29,7 @@ def em_worker_quality(rows):
 def em_votes(rows, worker_qual):
     pass
 
-def em_iteration(rows):
+def em_iteration(rows, worker_qual):
     labels = em_votes(rows, worker_qual)
     worker_qual = em_worker_quality(labels, rows)
     return labels, worker_qual
